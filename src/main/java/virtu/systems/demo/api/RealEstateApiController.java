f22ff6c702bd2d7530005acff1c9560a0e458a19
ZhevlakovAL <ZhevlakovAL@gmail.com>
2020-02-09 21:24:36 +0200

    types

--- src/main/java/virtu/systems/demo/api/RealEstateApiController.java
+++ src/main/java/virtu/systems/demo/api/RealEstateApiController.java
@@ -1,55 +1,24 @@
 package virtu.systems.demo.api;
 
-import virtu.systems.demo.api.dto.RealEstateTypesResponseDto;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.swagger.annotations.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
+import io.swagger.annotations.Api;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
-
-import javax.validation.constraints.*;
-import javax.validation.Valid;
-import javax.servlet.http.HttpServletRequest;
-import java.util.Optional;
-import java.io.IOException;
-import java.util.List;
+import virtu.systems.demo.api.dto.RealEstateTypesResponseDto;
+import virtu.systems.demo.config.JsonProperties;
+import virtu.systems.demo.map.RealEstateMapper;
 
 @Api(tags = "RealEstate")
 @Controller
 public class RealEstateApiController implements RealEstateApi {
 
-    private static final Logger log = LoggerFactory.getLogger(RealEstateApiController.class);
+    private final JsonProperties properties;
 
-    private final ObjectMapper objectMapper;
-
-    private final HttpServletRequest request;
-
-    @org.springframework.beans.factory.annotation.Autowired
-    public RealEstateApiController(ObjectMapper objectMapper, HttpServletRequest request) {
-        this.objectMapper = objectMapper;
-        this.request = request;
+    public RealEstateApiController(final JsonProperties properties) {
+        this.properties = properties;
     }
 
     public ResponseEntity<RealEstateTypesResponseDto> getRealEstateTypes() {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<RealEstateTypesResponseDto>(objectMapper.readValue("{  \"types\" : [ \"Квартира\", \"Дом\", \"Комната\" ]}", RealEstateTypesResponseDto.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<RealEstateTypesResponseDto>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        return new ResponseEntity<RealEstateTypesResponseDto>(HttpStatus.NOT_IMPLEMENTED);
+        return ResponseEntity.ok(RealEstateMapper.INSTANCE.toResponse(properties.getTypes()));
     }
 
 }
--- src/main/java/virtu/systems/demo/map/RealEstateMapper.java
+++ src/main/java/virtu/systems/demo/map/RealEstateMapper.java
@@ -3,7 +3,10 @@ package virtu.systems.demo.map;
 import org.mapstruct.factory.Mappers;
 import virtu.systems.demo.api.dto.RealEstateCreateDto;
 import virtu.systems.demo.api.dto.RealEstateDto;
+import virtu.systems.demo.api.dto.RealEstateTypesResponseDto;
 import virtu.systems.demo.dao.entity.RealEstate;
+
+import java.util.List;
 
 @org.mapstruct.Mapper
 public interface RealEstateMapper {
@@ -17,4 +20,8 @@ public interface RealEstateMapper {
     RealEstateDto toDto(RealEstate realEstate);
 
     RealEstate toDao(RealEstateDto realEstateDto);
+
+    default RealEstateTypesResponseDto toResponse(List<String> types) {
+        return new RealEstateTypesResponseDto().types(types);
+    }
 }
